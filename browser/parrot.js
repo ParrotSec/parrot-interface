44f08c0f1d268b6a3df0c3729e29b89c9adbd1a7
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2020-02-20 23:37:10 +0100

    Import Debian version 4.9+parrot2
    parrot-interface (4.9+parrot2) rolling-testing; urgency=medium
    
      * Disable telemetry in firefox.
      * Add missing bookmarks.
      * Remove amazon shit from firefox configs.
      * Enable tracking protection when possible.
      * Fix policies.json firefox path.

--- browser/parrot.js
+++ browser/parrot.js
@@ -7,6 +7,40 @@ lockPref("app.update.enabled", false);
 lockPref("app.update.auto", false);
 lockPref("app.update.mode", 0);
 lockPref("app.update.service.enabled", false);
+lockPref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+lockPref("browser.newtabpage.activity-stream.telemetry", false);
+lockPref("browser.newtabpage.activity-stream.telemetry.structuredIngestion", false);
+lockPref("browser.newtabpage.activity-stream.telemetry.structuredIngestion.endpoint", "");
+lockPref("browser.ping-centre.telemetry", false);
+lockPref("browser.urlbar.eventTelemetry.enabled", false);
+lockPref("security.app_menu.recordEventTelemetry", false);
+lockPref("security.identitypopup.recordEventTelemetry", false);
+lockPref("security.protectionspopup.recordEventTelemetry", false);
+lockPref("toolkit.telemetry.archive.enabled", false);
+lockPref("toolkit.telemetry.bhrPing.enabled", false);
+lockPref("toolkit.telemetry.ecosystemtelemetry.enabled", false);
+lockPref("toolkit.telemetry.firstShutdownPing.enabled", false);
+lockPref("toolkit.telemetry.newProfilePing.enabled", false);
+lockPref("toolkit.telemetry.server", "");
+lockPref("toolkit.telemetry.shutdownPingSender.enabled", false);
+lockPref("toolkit.telemetry.unified", false);
+lockPref("toolkit.telemetry.updatePing.enabled", false);
+lockPref("browser.shell.checkDefaultBrowser", false);
+pref("browser.startup.firstrunSkipsHomepage", false);
+pref("browser.startup.homepage", "https://start.parrot.sh");
+pref("startup.homepage_welcome_url", "https://docs.parrotsec.org/donate");
+pref("browser.newtabpage.activity-stream.default.sites", "https://www.parrotsec.org/,https://crypt.parrot.sh,https://community.parrotsec.org,https://www.hackthebox.eu/,https://riot.im/app/,https://librem.one/,https://nextcloud.com/signup/,https://www.linode.com/?r=2b6d749d3cb301e41bdbec5fc40e50c4316f7c66");
+pref("browser.newtabpage.pinned", "[{\"url\":\"https://crypt.parrot.sh\",\"label\":\"CryptPad\",\"searchTopSite\":true}]");
+pref("browser.search.hiddenOneOffs", "Bing,Amazon.com,eBay,Twitter");
+pref("browser.search.suggest.enabled", false);
+pref("browser.urlbar.placeholderName", "DuckDuckGo");
+pref("browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.havePinned", "google");
+pref("browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.searchEngines", "google");
+pref("privacy.trackingprotection.socialtracking.enabled", true);
+pref("privacy.trackingprotection.origin_telemetry.enabled", true);
+pref("privacy.trackingprotection.enabled", true);
+pref("privacy.resistFingerprinting", true);
+pref("privacy.donottrackheader.enabled", true);
 // Don't show 'know your rights' on first run
 pref("browser.rights.3.shown", true);
 // Don't show WhatsNew on first run after every update
@@ -27,3 +61,4 @@ pref("network.proxy.socks_port", 9050);
 pref("network.proxy.type", 2);
 // set default search engine
 pref("browser.search.defaultenginename", "DuckDuckGo");
+
